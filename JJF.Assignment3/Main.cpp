21c6ced0084826e5238cc0ef23344dc783c636dc
Jordan Fredericks <500172971@fvtc.edu>
2024-09-11 19:47:20 -0500

    Update controls added

--- JJF.Assignment3/Main.cpp
+++ JJF.Assignment3/Main.cpp
@@ -18,6 +18,8 @@ struct Book
 
 int main()
 {
+	//create the array and add it's values
+
 	const int NUM_BOOKS = 4;
 	Book books[NUM_BOOKS] = {
 		{ "Dune", "Unknown", 0, 0 },
@@ -26,6 +28,8 @@ int main()
 		{ "Ender's Game", "Orson Scott Card", 1985, 324 }
 	};
 
+	//Display every book and it's information
+
 	for (int i = 0; i < NUM_BOOKS; i++)
 	{
 		cout << i + 1 << ". " << books[i].Title << "\n";
@@ -33,6 +37,31 @@ int main()
 		cout << "Year Published: " << books[i].YearPublished << "\n";
 		cout << "Number of Pages: " << books[i].NumPages << "\n\n";
 	}
+
+	//Update the books information
+
+	int ID = 0;
+	string tempAuthor;
+	int tempYear;
+	int tempPage;
+	char YN = 'n'; //Yes or no to save changes
+	cout << "Enter the ID of the book you would like to update (1-" << NUM_BOOKS << "): ";
+	cin >> ID;
+	cout << ID << ". " << books[ID - 1].Title << "\n";
+	cout << "Enter the Author (" << books[ID - 1].Author << "): ";
+	cin >> tempAuthor;
+	cout << "Enter the Year Published: (" << books[ID - 1].YearPublished << "): ";
+	cin >> tempYear;
+	cout << "Enter the Number of Pages: (" << books[ID - 1].NumPages << "): ";
+	cin >> tempPage;
+	
+	//Ensures user enters y or n
+
+	/*while (YN != 'y' || YN != 'n')
+	{
+		cout << "\nSave these changes? (Y/N): ";
+		cin >> YN;
+	}*/
 
 	(void)_getch();
 	return 0;
